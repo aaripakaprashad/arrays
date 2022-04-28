2bcebde1ab9cc45022882f29f9266675576395cd
prashad <63783349+aaripakaprashad@users.noreply.github.com>
2022-04-28 11:36:26 +0530

    Update eliminate_prime_from_array.c

--- eliminate_prime_from_array.c
+++ eliminate_prime_from_array.c
@@ -19,13 +19,24 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 int is_prime(int n)
 {
-    if(n==1)
-       return 0; 
-    if(n==2)
+    if(n==1) {
+       printf("n==1 , return 0\n");
+       return 0; //not a prime number
+    }
+    if((n==2) || (n ==3)){
        return 1;
+    }
     for(int i=2; i<n; i++) {
         if((n%i) == 0)
            return 0; //not prime number
@@ -38,15 +49,18 @@ int is_prime(int n)
 int main()
 {
     printf("Hello World");
-    int a[10] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50}; 
+    int a[50] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50}; //o/p:{4,6,8,9,10}    
     int size,len;
-    size =  len = sizeof(a)/sizeof(a[0]);
-    for(int i=0; i< size ; i++) {
+    size = len = sizeof(a)/sizeof(a[0]);
+    for(int i=0; i<size; i++) {
+        printf("checking a[%d]:%d\n",i,a[i]);
         if(is_prime(a[i])) {
+            printf("%d is prime number \n",a[i]);
             len--;
-            for(int j=i; j< size; j++){
+            for(int j=i; j<size; j++){
                 a[j] = a[j+1];
             }
+            i--;
         }
     }
     
@@ -54,3 +68,4 @@ int main()
        printf("%d, ",a[i]);
     return 0;
 }
+// o/p: 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50,
