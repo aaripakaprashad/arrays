315fa113c4a1f9a7ba74503b956e3c64cb86da52
prashad <63783349+aaripakaprashad@users.noreply.github.com>
2022-04-28 11:15:12 +0530

    Update eliminate_prime_from_array.c

--- eliminate_prime_from_array.c
+++ eliminate_prime_from_array.c
@@ -39,7 +39,8 @@ int main()
 {
     printf("Hello World");
     int a[10] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50}; 
-        int size = int len = sizeof(a)/sizeof(a[0]);
+    int size,len;
+    size =  len = sizeof(a)/sizeof(a[0]);
     for(int i=0; i< size ; i++) {
         if(is_prime(a[i])) {
             len--;
